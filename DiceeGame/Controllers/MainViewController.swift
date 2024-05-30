d9bf5073f995532ff6ada5796b317c5148b87e01
GarifullinII <ildaridrisovich@gmail.com>
2024-05-30 17:55:41 +0400

    change extensions, add images

--- DiceeGame/Controllers/MainViewController.swift
+++ DiceeGame/Controllers/MainViewController.swift
@@ -8,14 +8,36 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    private let logoImageView:  UIImageView = {
+        let image = UIImage(named: "diceeLogo.png")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        view.backgroundColor = .red
+        setupViews()
+        setConstraints()
     }
+    
+    private func setupViews() {
+        view.backgroundColor = .specialBackground
+        view.addSubview(logoImageView)
+    }
+}
 
-
+extension MainViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            logoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            logoImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 300)
+        ])
+    }
 }
 
--- DiceeGame/Extensions/UIKit/UIColor + Extensions.swift
+++ DiceeGame/Extensions/UIKit/UIColor + Extensions.swift
@@ -1,12 +1,15 @@
 //
 //  UIColor + Extensions.swift
-//  Joke
+//  DiceeGame
 //
-//  Created by Ildar Garifullin on 27/05/2024.
+//  Created by Ildar Garifullin on 30/05/2024.
 //
 
 import UIKit
 
+import UIKit
+
 extension UIColor {
-    static let specialBackground = #colorLiteral(red: 0.6334275007, green: 1, blue: 0, alpha: 1)
+    static let specialBackground = #colorLiteral(red: 0.07670832425, green: 0.4841311574, blue: 0.3039500117, alpha: 1)
 }
+
